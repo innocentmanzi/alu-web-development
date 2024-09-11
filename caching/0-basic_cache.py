320e2ad2dc77f81ec87db2b0dd20e9406c2a8afe
innocentmanzi <i.manzi@alustudent.com>
2024-09-11 00:36:05 -0700

    Basic dictionary

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,31 +1,21 @@
 #!/usr/bin/python3
-""" 0-main """
+"""Basic cache"""
 
-# Assuming BaseCaching is defined somewhere, we inherit from it
-class BaseCaching:
-    def __init__(self):
-        """Initialize the base cache"""
-        self.cache_data = {}
+BaseCaching = __import__('base_caching').BaseCaching
+
 
 class BasicCache(BaseCaching):
-    def __init__(self):
-        """Initialize the BasicCache"""
-        super().__init__()  # Call the parent class initializer
+    """Basic cache"""
 
     def put(self, key, item):
-        """
-        Assign the item value to the key in self.cache_data.
-        If key or item is None, do nothing.
-        """
-        if key is not None and item is not None:
-            self.cache_data[key] = item
+        """Add an item in the cache"""
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
 
     def get(self, key):
-        """
-        Retrieve the value in self.cache_data associated with the key.
-        Return None if the key is None or doesn't exist.
-        """
-        if key is None:
+        """Get an item by key"""
+        if key is None or key not in self.cache_data:
             return None
-        return self.cache_data.get(key, None)
+        return self.cache_data[key]
 
